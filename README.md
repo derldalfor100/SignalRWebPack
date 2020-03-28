040a3cac1fe7e114168494d642eca9070194e63a
derldalfor100 <derldalfor100@gmail.com>
2020-03-28 21:35:44 +0300

    add init static files

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# SignalRWebPack
+https://docs.microsoft.com/en-us/aspnet/core/tutorials/signalr-typescript-webpack?view=aspnetcore-2.1&tabs=visual-studio
---
+++ SignalRWebPack/src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("./css/main.css");
+var divMessages = document.querySelector("#divMessages");
+//# sourceMappingURL=index.js.map
