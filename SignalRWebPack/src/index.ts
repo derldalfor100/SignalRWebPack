416ecb79d872a818ea1c528f9b3113e4c077fef3
derldalfor100 <derldalfor100@gmail.com>
2020-04-04 15:42:36 +0300

    add send message methods

--- SignalRWebPack/src/index.ts
+++ SignalRWebPack/src/index.ts
@@ -1,4 +1,5 @@
 ﻿import "./css/main.css";
+import * as signalR from "@aspnet/signalr";
 
 const divMessages: HTMLDivElement = document.querySelector("#divMessages");
 
@@ -7,6 +8,21 @@ const tbMessage: HTMLInputElement = document.querySelector("#tbMessage");
 const btnSend: HTMLButtonElement = document.querySelector("#btnSend");
 
 const username = new Date().getTime();
+
+const connection = new signalR.HubConnectionBuilder().withUrl("/hub").build();
+
+connection.on("messageReceived", (username: string, message: string) => {
+
+    const m = document.createElement("div");
+
+    m.innerHTML = `<div class="message-author">${username}</div><div>${message}</div>`;
+
+    divMessages.appendChild(m);
+
+    divMessages.scrollTop = divMessages.scrollHeight;
+});
+
+connection.start().catch(err => document.write(err));
 
 tbMessage.addEventListener("keyup", (e: KeyboardEvent) => {
     if (e.keyCode === 13) {
@@ -18,4 +34,6 @@ btnSend.addEventListener("click", send);
 
 function send() {
 
+    connection.send("newMessage", username, tbMessage.value)
+        .then(() => tbMessage.value = "");
 }
